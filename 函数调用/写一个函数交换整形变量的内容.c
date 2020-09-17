79c13e9d41a3b9c23e730946ddc652229814c8e2
hu <hurk7106235414@163.com>
2020-09-17 17:13:58 +0800

    Create 写一个函数交换整形变量的内容.c

---
+++ 函数调用/写一个函数交换整形变量的内容.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//int swap( int* a, int* b ) {
+//	int c = 2;
+//	c = *a;
+//	*a = *b;
+//	*b = c;
+//}	
+//
+//int main() {
+//	int x = 0;
+//	int y = 1;
+//	//a = &x;
+//	//b = &y;
+//	swap( &x, &y);
+//	printf("%d%d", x, y);
+//	return 0;
+//}
