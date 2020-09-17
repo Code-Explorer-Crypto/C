7c44396f6ee1a4839950c553b71b154ce5cbd730
hu <hurk7106235414@163.com>
2020-09-17 17:14:03 +0800

    Create 判断是不是闰年.c

---
+++ 函数调用/判断是不是闰年.c
@@ -0,0 +1,30 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int Is(x) {
+//	if (x % 100 == 0) {
+//		if (x % 400 == 0) {
+//			return 1;
+//		}
+//		else
+//			return 0;
+//	}
+//	else {
+//		if (x % 4 == 0) {
+//			return 1;
+//		}
+//		else
+//			return 0;
+//	}
+//}
+//
+//int main() {
+//	int x = 0;
+//	printf("请输入年份:");
+//	scanf("%d", &x);
+//	int b = Is(x);
+//	if (b) {
+//		printf("是闰年");
+//	}
+//	else
+//		printf("不是闰年");
+//}
