394f5bb2df742957968069ab833546ad55d19196
hu <hurk7106235414@163.com>
2020-09-18 09:38:38 +0800

    Update 整形有序数组的二分查找.c

--- 函数调用/整形有序数组的二分查找.c
+++ 函数调用/整形有序数组的二分查找.c
@@ -1,30 +1,58 @@
-//二分查找(数字从小到大排列):
+////二分查找(数字从小到大排列):
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//int BinarySearch( int arr[], int size,int a) {
+//	int left = 0;
+//	int right = size - 1;
+//	while (left <= right) {
+//		int mid = (right-left) / 2;
+//		if (arr[mid] < a) {
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > a) {
+//			right = mid - 1;
+//		}
+//		else {
+//			printf("找到了,下标为%d", mid);
+//			break;
+//		}	
+//	}
+//}
+//
+//int main() {
+//	int arr[] = { 0, 1, 2, 3, 4, 5 };
+//	int a = 2;
+//	int size = sizeof(arr) / sizeof(arr[0]);
+//	BinarySearch( arr, size, a );
+//	return 0;
+//  }
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-int HalfFind( arr[], size, a) {
+int BinarySearch(int arr[], int size, int a) {
 	int left = 0;
 	int right = size - 1;
-	while (left < right) {
-		int mid = (right-left) / 2;
+	while (left <= right) {
+		int mid = (left + right) / 2;
 		if (arr[mid] < a) {
-			left = mid;
+			left = mid + 1;
 		}
 		else if (arr[mid] > a) {
-			right = mid;
+			right = mid - 1;
 		}
 		else {
 			printf("找到了,下标为%d", mid);
 			break;
 		}
 	}
-	printf("没找到");
 	return 0;
-}
+   }
 
 int main() {
-	int arr[] = { 0, 1, 2, 3, 4, 5};
-	int a = 2;
+	int arr[] = { 1, 2, 3, 4 };
 	int size = sizeof(arr) / sizeof(arr[0]);
-	HalfFind();
-  }
+	int a = 2;
+	BinarySearch(arr, size, a);
+}
