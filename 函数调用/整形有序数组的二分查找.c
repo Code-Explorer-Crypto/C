55d7fd3efdab330d97da11b56fd9882cacba0c46
hu <hurk7106235414@163.com>
2020-09-18 11:02:58 +0800

    Update 整形有序数组的二分查找.c

--- 函数调用/整形有序数组的二分查找.c
+++ 函数调用/整形有序数组的二分查找.c
@@ -27,32 +27,32 @@
 //	BinarySearch( arr, size, a );
 //	return 0;
 //  }
-
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <string.h>
-int BinarySearch(int arr[], int size, int a) {
-	int left = 0;
-	int right = size - 1;
-	while (left <= right) {
-		int mid = (left + right) / 2;
-		if (arr[mid] < a) {
-			left = mid + 1;
-		}
-		else if (arr[mid] > a) {
-			right = mid - 1;
-		}
-		else {
-			printf("找到了,下标为%d", mid);
-			break;
-		}
-	}
-	return 0;
-   }
-
-int main() {
-	int arr[] = { 1, 2, 3, 4 };
-	int size = sizeof(arr) / sizeof(arr[0]);
-	int a = 2;
-	BinarySearch(arr, size, a);
-}
+//
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//int BinarySearch(int arr[], int size, int a) {
+//	int left = 0;
+//	int right = size - 1;
+//	while (left <= right) {
+//		int mid = (left + right) / 2;
+//		if (arr[mid] < a) {
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > a) {
+//			right = mid - 1;
+//		}
+//		else {
+//			printf("找到了,下标为%d", mid);
+//			break;
+//		}
+//	}
+//	return 0;
+//   }
+//
+//int main() {
+//	int arr[] = { 1, 2, 3, 4 };
+//	int size = sizeof(arr) / sizeof(arr[0]);
+//	int a = 2;
+//	BinarySearch(arr, size, a);
+//}
