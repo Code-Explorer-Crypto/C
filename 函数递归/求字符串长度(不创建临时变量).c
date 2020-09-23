ea721afe895b6c465d229b6843f2dd2ebda5b4a9
hu <hurk7106235414@163.com>
2020-09-23 16:21:56 +0800

    Create 求字符串长度(不创建临时变量).c

---
+++ 函数递归/求字符串长度(不创建临时变量).c
@@ -0,0 +1,15 @@
+//#include <stdio.h>
+//#include <string.h>
+//int Strlen(char str[]) {
+//	if (str[0] == '\0') {
+//	return 0;	
+//	}
+//	return 1 + Strlen(str + 1);
+//  }
+//
+//int main() {
+//	char str[] = "abcd";
+//	int len = Strlen(str);
+//	printf("%d", len);
+//	return 0;
+//}	
