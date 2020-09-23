0a7c6d7a8b73890415edd9c511e38063812f80b4
hu <hurk7106235414@163.com>
2020-09-23 16:22:26 +0800

    Create 第五题.c

---
+++ 数学/第五题.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//    char str[] = "hello bit";
+//    int size = sizeof(str) / sizeof(str[0]);
+//    for (int i = 0; i <= size; i++) {
+//        if (str[i] == '\n') {
+//            break;
+//        }
+//        else
+//            printf("%s", str[i]);
+//    }
+//    Reverse();
+//    return 0;
+//}
