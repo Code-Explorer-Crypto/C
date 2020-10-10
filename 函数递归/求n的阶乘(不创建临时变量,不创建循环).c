6668972b895fc7696cb5e9a83e3cbcc9442d7509
hu <hurk7106235414@163.com>
2020-10-10 18:06:20 +0800

    Update 求n的阶乘(不创建临时变量,不创建循环).c

--- 函数递归/求n的阶乘(不创建临时变量,不创建循环).c
+++ 函数递归/求n的阶乘(不创建临时变量,不创建循环).c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-int Factor(int a) {
-	if (a != 1) {
-		return a * Factor(a - 1);
-	}
-	return 1;
-  }
-	
-
-int main() {
-	int Mul = Factor(5);
-	printf("%d", Mul);
-	return 0;
-}
+//#include <stdio.h>
+//int Factor(int a) {
+//	if (a != 1) {
+//		return a * Factor(a - 1);
+//	}
+//	return 1;
+//  }
+//	
+//
+//int main() {
+//	int Mul = Factor(5);
+//	printf("%d", Mul);
+//	return 0;
+//}
