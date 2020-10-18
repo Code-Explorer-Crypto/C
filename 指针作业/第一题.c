8af97a0abc6b0a300c9f75ea497a63177042b0b9
hu <hurk7106235414@163.com>
2020-10-18 11:52:51 +0800

    Create 第一题.c

---
+++ 指针作业/第一题.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//    int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//    int* a = &arr;
+//    int size = sizeof(arr) / sizeof(arr[0]) - 1;
+//    for (int i = 0;i < size; i++) {
+//        printf("%d ", *a);
+//    }
+//    return 0;
+//}
