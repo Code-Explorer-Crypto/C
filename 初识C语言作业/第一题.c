a8fa7a777af52992af40ac81e90047a9b493c8ae
hu <hurk7106235414@163.com>
2020-10-18 11:52:16 +0800

    Create 第一题.c

---
+++ 初识C语言作业/第一题.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//int get_max(num1, num2) {
+//	if (num1 > num2) {
+//		return num1;
+//	}
+//	return num2;
+//}
+//
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	//要求：完成get_max函数
+//	int max = get_max(num1, num2);
+//	printf("max = %d\n", max);
+//	return 0;
+//}
