4e8be3c05fa674b8a1c516fa170f13dfb57b5bc5
hu <hurk7106235414@163.com>
2020-10-18 11:52:55 +0800

    Create 第三题.c

---
+++ 指针作业/第三题.c
@@ -0,0 +1,13 @@
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	char str[] = "hello bit";
+//	//在这里完成下面函数，参数自己设计，要求：使用指针
+//	int size = sizeof(str) / sizeof(str[0]);
+//	char* a = &str;
+//	for (int i = size - 1;i >= 0; i = i - 1) {
+//		printf("%s", *(a + i));
+//	}
+//	return 0;
+//}
