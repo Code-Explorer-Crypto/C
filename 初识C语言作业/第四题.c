cbb77181ae3ae957161985bd29057f738fc4d0cf
hu <hurk7106235414@163.com>
2020-10-19 10:13:55 +0800

    Update 第四题.c

---
+++ 初识C语言作业/第四题.c
@@ -0,0 +1,19 @@
+//#include <stdio.h>
+//int num = 10;
+//int main()
+//{
+//    int num = 1;
+//    printf("num = %d\n", num);
+//    return 0;
+//}
+//
+//
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//    char arr[] = { 'b', 'i', 't' };
+//    printf("%d \b", strlen(arr));
+//    return 0;
+//}
