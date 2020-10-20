56582ea3b386cc1603fa3663de528fdfa109572c
hu <hurk7106235414@163.com>
2020-10-20 11:07:25 +0800

    Create 第七题.c

---
+++ 初识C语言作业/第七题.c
@@ -0,0 +1,34 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main() {
+//	int arr[9][9] = { 0 };
+//	for (int i = 0;i < 9;i++){
+//		for (int j = 0;j < 9;j++) {
+//			 arr[i][j] = (i + 1) * (j + 1);
+//			 printf("%d*%d=%d ", (i + 1), (j + 1), arr[i][j]);
+//			}
+//		printf("\n");
+//	}
+//	printf("%d", arr[9][9]);
+//	return 0;
+//   }
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	//控制行数
+//	for (i = 1; i <= 9; i++)
+//	{
+//		//打印每一行内容，每行有i个表达式
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%2d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
