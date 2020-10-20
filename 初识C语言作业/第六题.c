eaeaad80db626c0593cba3084798637ee5b8dd8b
hu <hurk7106235414@163.com>
2020-10-20 11:07:20 +0800

    Update 第六题.c

--- 初识C语言作业/第六题.c
+++ 初识C语言作业/第六题.c
@@ -46,10 +46,10 @@
 //	float sum1 = 0;
 //	float sum2 = 0;
 //	for (float i = 1;i <= 100;i+=2) {
-//		sum1 = sum1 + 1.0 / i;
+//		sum1 = (float)(sum1 + 1.0 / i);
 //	}
 //	for (float i = 2;i <= 100;i += 2) {
-//		sum2 = sum2 - 1.0 / i;
+//		sum2 = (float)(sum2 - 1.0 / i);
 //	}
 //	sum = sum1 + sum2;
 //	printf("%f", sum);
@@ -64,20 +64,45 @@
 // }
 
 
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-int main() {
-	int a = 0,b = 0,c = 0,d = 0,e = 0;
-	int f = 0,g = 0,h = 0,i = 0,j = 0;
-	int tem = 0;
-	scanf("%d%d%d%d%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f, &g, &h, &i, &j);
-	int arr[] = { a, b ,c ,d ,e ,f ,g, h ,i ,j };
-	for (int i = 9;i >= 0;i--) {
-		if (arr[i] > arr[i - 1]) {
-			tem = arr[i];
-			arr[i] = arr[i - 1];
-			arr[i - 1] = tem;
-		}
-	}
-	printf("%d", arr[0]);
-  }
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main() {
+//	int a = 0,b = 0,c = 0,d = 0,e = 0;
+//	int f = 0,g = 0,h = 0,i = 0,j = 0;
+//	int tem = 0;
+//	scanf("%d%d%d%d%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f, &g, &h, &i, &j);
+//	int arr[] = { a, b ,c ,d ,e ,f ,g, h ,i ,j };
+//	for (int x = 9;x >= 1;x--) {
+//		if (arr[x] > arr[x - 1]) {
+//			tem = arr[x];
+//			arr[x] = arr[x - 1];
+//			arr[x - 1] = tem;
+//		}
+//	}
+//	printf("%d", arr[0]);
+//  }
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	int max = 0;
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	//
+//	max = arr[0];
+//	for (i = 1; i < 10; i++)
+//	{
+//		if (arr[i] > max)
+//			max = arr[i];
+//	}
+//	printf("max = %d\n", max);
+//	return 0;
+//}
+
+
