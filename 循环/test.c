bbeb1d367a13641a3e749c866591bbe7bdd4757d
hu <hurk7106235414@163.com>
2020-10-21 10:41:05 +0800

    Create test.c

---
+++ 循环/test.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main() {
+//	int  a = 0;
+//	scanf("%d", &a);
+//	switch (a){
+//	case(1):
+//		printf("1");
+//		break;
+//	case(2):
+//		printf("2");
+//		break;
+//	case(3):
+//		printf("3");
+//		break;
+//	default:
+//		printf("ERROR");
+//		break;
+//}
+// }
