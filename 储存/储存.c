c03944bea41fcf01dd7501d138ff14e4856be6e0
hu <hurk7106235414@163.com>
2020-10-25 15:10:57 +0800

    Create 储存.c

---
+++ 储存/储存.c
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main() {
+//	char a = 0;
+//	short b = 0;
+//	int c = 0;
+//	long d = 0;
+//	long long e = 0;
+//	float f = 0;
+//	double g = 0;
+//	printf("char %d\n", sizeof(a));
+//	printf("short %d\n", sizeof(b));
+//	printf("int %d\n", sizeof(c));
+//	printf("long %d\n", sizeof(d));
+//	printf("long long %d\n", sizeof(e));
+//	printf("float %d\n", sizeof(f));
+//	printf("double %d\n", sizeof(g));
+//   }
+
