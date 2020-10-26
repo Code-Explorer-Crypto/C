8380f24f5cf71db3d62104da11fb88d95929ba1a
hu <hurk7106235414@163.com>
2020-10-26 18:28:26 +0800

    Create strcmp.c

---
+++ str函数/strcmp.c
@@ -0,0 +1,20 @@
+//#define	 _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//int main() {
+//	char arr1[] = { "xxxxxxxxxxxxxxxx" };
+//	char arr2[] =  "abcde" ;
+//	strcpy(arr1,arr2);
+//	printf("%s", arr1);
+//  }
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//int main() {
+//	char arr1[] = { "xxxxxxxxxxxxxxxx" };
+//	char arr2[5] = { "abcde" };
+//	strcpy(arr1, arr2);
+//	printf("%s", arr1);
+//   }
+
