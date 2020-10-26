a278d529f96e2bc0435ad2feb58b58a696c42cba
hu <hurk7106235414@163.com>
2020-10-26 18:28:30 +0800

    Create strlen.c

---
+++ str函数/strlen.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//#include <string.h>
+//int my_strlen(char* arr) {
+//	int count = 0;
+//	while (*arr) {
+//		arr++;
+//		count++;
+//	}
+//	return count;
+//}
+//
+//int main() {
+//	char arr1[] = "abcde";
+//	my_strlen(arr1);
+//	printf("%d", my_strlen(arr1));
+//   }
