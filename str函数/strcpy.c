63ff575f13b127b55f60330b80fcb0966b0140fa
hu <hurk7106235414@163.com>
2020-10-26 18:28:28 +0800

    Create strcpy.c

---
+++ str函数/strcpy.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//#include <assert.h>
+//int my_strcpy(char* arr1, char* arr2) {
+//	assert(arr1 != NULL);
+//	assert(arr2 != NULL);
+//	while (*arr1 = *arr2) {
+//		arr1++;
+//		arr2++;
+//	}
+//	return arr1;
+//}
+//
+//int main() {
+//	char arr1[] = {'0','0', '0', '0', '0', '0','0'};
+//	char arr2[] = "abcde";
+//	my_strcpy(arr1, arr2);
+//	printf("%s", arr1);
+//}
