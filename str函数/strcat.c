6235a9c6599a5e6800b1ba6663cb12d7a45704ed
hu <hurk7106235414@163.com>
2020-10-29 10:23:34 +0800

    Update strcat.c

--- str函数/strcat.c
+++ str函数/strcat.c
@@ -1,20 +1,32 @@
-#include <stdio.h>
-#include <string.h>
-#include <assert.h>
-char my_strcat(char* arr1, char* arr2) {
-	assert(arr1 != NULL);
-	assert(arr2 != NULL);
-	char* ret = arr1;
-	while (*arr1) {
-		arr1++;
-	}
-	while (*arr1++ = *arr2++) {
-	}
-	return ret;
-}
+//#include <stdio.h>
+//#include <string.h>
+//#include <assert.h>
+//char my_strcat(char* arr1, char* arr2) {
+//	assert(arr1 != NULL);
+//	assert(arr2 != NULL);
+//	char* ret = arr1;
+//	while (*arr1) {
+//		arr1++;
+//	}
+//	while (*arr1++ = *arr2++) {
+//	}
+//	return ret;
+//}
+//
+//int main() {
+//	char arr1[9] = "jkl";
+//	char arr2[] = "abcde";
+//	my_strcat(arr1, arr2);
+//	printf("%s",arr1);
+// }
+//
 
-int main() {
-	char arr1[20] = "jkl";
-	/*char arr2[] = "abcde";*/
-	printf("%s", my_strcat(arr1, "abcde"));
- }
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//int main() {
+//	char arr1[20] = "abcde";
+//	char arr[] = "fgh";
+//	printf("%s", strcat(arr1, arr1));
+//   }
+
