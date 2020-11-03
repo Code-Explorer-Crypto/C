ac6813b92fc2837212a0373446472d0697c3e37f
hu <hurk7106235414@163.com>
2020-11-03 11:51:00 +0800

    Update memmove.c

--- mem/memmove.c
+++ mem/memmove.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+//void* my_memmove(void* des, void* src, size_t size) {
+//	void* ret = des;
+//	if (src < des) {
+//		while(size--){
+//		*(char*)des = *(char*)src;
+//		des = ((char*)des)++;
+//		src = ((char*)src)++;
+//		}
+//	}
+//	else if (src >= des) {
+//		while (size--) {
+//			*((char*)des + size) = *((char*)src + size);
+//		}
+//	}
+//	return ret;
+//}
+//
+//int main() {
+//	char arr1[] = "abcedeg";
+//	printf("%s", my_memmove(arr1, "lkj", 3));
+//  }
+
+
+
 int main() {
-	memmove();
-  }
+	printf("%d", sizeof(long));
+}
