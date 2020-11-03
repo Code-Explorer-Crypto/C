a97f969b0270e81a51e129f1a44a5761e74957e6
hu <hurk7106235414@163.com>
2020-11-03 11:51:23 +0800

    Update strcpy.c

--- str函数/strcpy.c
+++ str函数/strcpy.c
@@ -13,8 +13,39 @@
 //}
 //
 //int main() {
-//	char arr1[] = {'0','0', '0', '0', '0','0'};
+//	char arr1[20] = "ghi";
 //	char arr2[] = "abcde\0f";
 //	my_strcpy(arr1, arr2);
 //	printf("%s", arr1);
 //}
+
+
+//#define	 _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//int main() {
+//	char arr1[] = { "xxxxxxxxxxxxxxxx" };
+//	char arr2[] =  "abcde" ;
+//	strcpy(arr1,arr2);
+//	printf("%s", arr1);
+//  }
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//int main() {
+//	char arr1[] = { "xxxxxxxxxxxxxxxx" };
+//	char arr2[5] = { "abcde" };
+//	strcpy(arr1, arr2);
+//	printf("%s", arr1);
+//   }
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//int main() {
+//	char arr1[20] = "abc";
+//	char arr2[] = "ejia";
+//	strncpy(arr1, arr2,5);
+//	printf("%s", arr1);
+//  }
