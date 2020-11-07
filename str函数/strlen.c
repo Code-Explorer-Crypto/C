872ed45ca539cbb043b80b646e538eefe358991e
hu <hurk7106235414@163.com>
2020-11-07 11:45:15 +0800

    Update strlen.c

--- str函数/strlen.c
+++ str函数/strlen.c
@@ -1,6 +1,6 @@
 //#include <stdio.h>
 //#include <string.h>
-//int my_strlen(char* arr) {
+//int my_strlen(const char* arr) {
 //	int count = 0;
 //	while (*arr) {
 //		arr++;
@@ -14,3 +14,21 @@
 //	my_strlen(arr1);
 //	printf("%d", my_strlen(arr1));
 //   }
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char str[] = "- This, a sample string.";
+	char* pch;
+	printf("Splitting string \"%s\" into tokens:\n", str);
+	pch = strtok(str, " ,.-");
+	while (pch != NULL)
+	{
+		printf("%s\n", pch);
+		pch = strtok(NULL, " ,.-");
+	}
+	return 0;
+}
