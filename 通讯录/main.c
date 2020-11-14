0ad5931c76dedf16f6464356f7acd8cf904b4f79
hu <hurk7106235414@163.com>
2020-11-14 15:37:42 +0800

    Create main.c

---
+++ 通讯录/main.c
@@ -0,0 +1,45 @@
+//实现一个通讯录；
+//通讯录可以用来存储1000个人的信息，每个人的信息包括：姓名、性别、年龄、电话、住址
+//添加联系人信息
+//删除指定联系人信息
+//查找指定联系人信息
+//修改指定联系人信息
+//显示所有联系人信息
+//清空所有联系人
+//以名字排序所有联系人
+
+#include "contact.h"
+enum {
+	ADD,
+	DEL,
+	SEARCH,
+	REPEAR,
+	SHOW,
+	SORT
+};
+
+int main() {
+	menu();
+	int input = 0;
+	printf("请选择:");
+	scanf("%d", &input);
+	switch (input) {
+	case ADD:
+		add(&data);
+		break;
+	case DEL:
+		break;
+	case SEARCH:
+		break;
+	case REPEAR:
+		break;
+	case SHOW:	
+		break;
+	case SORT:	
+		break;
+	default:
+		printf("输入有误");
+	}
+    
+}
+
