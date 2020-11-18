6ee7be372c221f6c73df4ef0b5c18317e062f1dc
hu <hurk7106235414@163.com>
2020-11-18 11:17:44 +0800

    Update contact.c

--- 通讯录/contact.c
+++ 通讯录/contact.c
@@ -1,31 +1,41 @@
 #include "contact.h"
+//菜单
 void menu() {
-	printf("------按1添加联系人信息---------\n");
-	printf("------按2删除指定联系人信息------\n");
-	printf("------按2删除指定联系人信息-----\n");
-	printf("------按2查找指定联系人信息-----\n");
-	printf("------按2修改指定联系人信息-----\n");
-	printf("------按2显示指定联系人信息-----\n");
-	printf("------按2清空指定联系人信息-----\n");
-	printf("------以名字排序所有联系人------\n");
+	printf("------按ADD添加联系人信息------------\n");
+	printf("------按DEL删除指定联系人信息--------\n");
+	printf("------按SEARCH查找指定联系人信息-----\n");
+	printf("------按REPEAR修改指定联系人信息-----\n");
+	printf("------按SHOW显示指定联系人信息-------\n");
+	printf("------按CLEAR清空指定联系人信息------\n");
+	printf("------按SORT以名字排序所有联系人-----\n");
 };
 
-void add(Info* data) {
-	char a = 0;
-	char b = 0;
-	int c = 0;
-	char d = 0;
-	char e = 0;
-	int sz = 0;
+//添加联系人
+void add(Data* s) {
+	printf("%d", s->cap);
 	printf("请输入名字:");
-	scanf("%s", data[sz].name);
+	scanf("%s", s->data[s->cap]->name);
 	printf("请输入性别:");
-	scanf("%s", data[sz].sex);
+	scanf("%s", s->data[s->cap]->sex);
 	printf("请输入年龄:");
-	scanf("%d", data[sz].age);
+	scanf("%d", s->data[s->cap]->age);
 	printf("请输入电话:");
-	scanf("%s", data[sz].tele);
+	scanf("%s", s->data[s->cap]->tele);
 	printf("请输入地址:");
-	scanf("%s", data[sz].des);
-	sz++;
+	scanf("%s", s->data[s->cap]->des);
+	s->cap++;
+}
+
+//增容
+void capacity(Data *s) {
+	if ((s->cap) % 3 == 0) {
+	    Info* c = (Info*) realloc(s->data,(s->cap + 3) * sizeof(Info));
+		printf("增容成功");
+	}
+}
+
+//初始化通讯录
+Init(Data* s) {
+	s->cap = 0;
+	s->data[3]= (Info*) malloc(3*sizeof(Info)) ;
 }
