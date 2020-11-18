abbcaf3d4c8577104e171d6555decbb5fb414a36
hu <hurk7106235414@163.com>
2020-11-18 11:18:49 +0800

    Create test.c

---
+++ 通讯录/test.c
@@ -0,0 +1,18 @@
+//typedef struct PeoInfo
+//{
+//	char name[MAX_NAME];
+//	char tele[MAX_TELE];
+//	char addr[MAX_ADDR];
+//	char qq[MAX_QQ];
+//	char sex[MAX_SEX];
+//	short age;
+//}PeoInfo;
+//
+//
+////默认可以存放5个人的信息
+//typedef struct Contact
+//{
+//	PeoInfo* data;//数据
+//	int sz;//有效个数
+//	int capacity;//通讯当前的容量
+//}Contact;
