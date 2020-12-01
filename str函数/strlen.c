38f5a14c09ef32ed42d52f9d0c9c6ea8d4810f7a
hu <hurk7106235414@163.com>
2020-12-01 11:51:56 +0800

    Update strlen.c

--- str函数/strlen.c
+++ str函数/strlen.c
@@ -16,19 +16,19 @@
 //   }
 
 
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <string.h>
-int main()
-{
-	char str[] = "- This, a sample string.";
-	char* pch;
-	printf("Splitting string \"%s\" into tokens:\n", str);
-	pch = strtok(str, " ,.-");
-	while (pch != NULL)
-	{
-		printf("%s\n", pch);
-		pch = strtok(NULL, " ,.-");
-	}
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	char str[] = "- This, a sample string.";
+//	char* pch;
+//	printf("Splitting string \"%s\" into tokens:\n", str);
+//	pch = strtok(str, " ,.-");
+//	while (pch != NULL)
+//	{
+//		printf("%s\n", pch);
+//		pch = strtok(NULL, " ,.-");
+//	}
+//	return 0;
+//}
