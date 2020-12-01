e31e1e91bb0f1b0cc40a164f39eb8b967cad8f6c
hu <hurk7106235414@163.com>
2020-12-01 11:51:51 +0800

    Update strcat.c

--- str函数/strcat.c
+++ str函数/strcat.c
@@ -30,3 +30,20 @@
 //	printf("%s", strcat(arr1, arr1));
 //   }
 
+
+
+/* strcat example */
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char str[80];
+//	strcpy(str, "these ");
+//	strcat(str, "strings ");
+//	strcat(str, "are ");
+//	strcat(str, "concatenated.");
+//	puts(str);
+//	return 0;
+//}
