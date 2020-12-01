bdc6961aef8e773adf649c19f05907b6fae2fdfe
hu <hurk7106235414@163.com>
2020-12-01 11:51:53 +0800

    Update strcpy.c

--- str函数/strcpy.c
+++ str函数/strcpy.c
@@ -68,3 +68,18 @@
 //	printf("%s", strcpy(arr1, arr2));
 //}
 //
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main ()
+//{
+//  char str1[]="Sample string";
+//  char str2[40];
+//  char str3[40];
+//  strcpy (str3,"copy successful");
+// /* printf ("str1: %s\nstr2: %s\nstr3: %s\n",str1,str2,str3);*/
+//  printf("%c",strcpy(str2, str1));
+//  return 0;
+//}
