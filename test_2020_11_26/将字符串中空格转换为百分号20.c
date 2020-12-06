bb2aa0649fd6977c335de8be06f83955085f1481
hu <hurk7106235414@163.com>
2020-12-06 16:07:22 +0800

    Update 将字符串中空格转换为百分号20.c

--- test_2020_11_26/将字符串中空格转换为百分号20.c
+++ test_2020_11_26/将字符串中空格转换为百分号20.c
@@ -1,6 +1,6 @@
 ////将字符串中空格转换为"%20"
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
 //int numofspace(str){
 //	int q = 0;
 //	while (str != '\n') {
@@ -12,26 +12,26 @@
 //	return q;		
 //}
 
-int main() {
-	char str[20];
-	scanf("%s", str);
-	int d = 0;
-	int i = 0;
-		while (str[i+d] != '\n'&& (i+ d)<20) {
-			if(str[i+d] == ' '){
-				for (int e = 1;(i+d+2+e) < 20;e++) {
-					str[i + d +2+e] = str[]
-				}
-					str[i+d] = '%';
-					str[i+d+1] = '2';
-					str[i+d+2] = '0';
-					d+=2;
-					
-				}
-			putchar(str[i+d]);
-			i++;
-		}
-	}
+//int main() {
+//	char str[20];
+//	scanf("%s", str);
+//	int d = 0;
+//	int i = 0;
+//		while (str[i+d] != '\n'&& (i+ d)<20) {
+//			if(str[i+d] == ' '){
+//				for (int e = 1;(i+d+2+e) < 20;e++) {
+//					str[i + d + 2 + e] = str[];
+//				}
+//					str[i+d] = '%';
+//					str[i+d+1] = '2';
+//					str[i+d+2] = '0';
+//					d+=2;
+//					
+//				}
+//			putchar(str[i+d]);
+//			i++;
+//		}
+//	}
 
 //#include <stdio.h>
 //#include <ctype.h>
@@ -51,3 +51,8 @@ int main() {
 //    return 0;
 //}
 
+//int main() {
+//	char str[20];
+//	scanf("%s", str);
+//
+//}
