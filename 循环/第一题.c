6e195073d571aa3d4a093fab3a2523d49493d3ad
hu <hurk7106235414@163.com>
2020-12-08 15:47:22 +0800

    Update 第一题.c

--- 循环/第一题.c
+++ 循环/第一题.c
@@ -1,4 +1,22 @@
-#include <stdio.h>
-int main() {
+//#include <stdio.h>
+//int main() {
+//	int n = 5;
+//	int i = 1;
+//	while (i) {
+//		i = --n;
+//		printf("%d ", i);
+//		printf("%d\n", n);
+//	}
+//  }
 
-  }
+//#include <stdio.h>
+//int main() {
+//	int n = 5;
+//	int i = 1;
+//	while (--n) {
+//		n--;
+//		int i = n * (n + 1);
+//		printf("%d\n", n);
+//		printf("%d\n", i);
+//	}
+//}
