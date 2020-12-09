d9f5754fcf9af89c2f70d96dbc7c2883d076cf54
hu <hurk7106235414@163.com>
2020-12-09 09:57:29 +0800

    Update seqList.h

--- 顺序表/seqList.h
+++ 顺序表/seqList.h
@@ -2,22 +2,43 @@
 //静态顺序表
 #define MAX 100
 typedef int SQLDataType;
-struct seqList2{
+struct SeqList2{
 	SQLDataType data[MAX];  /*不同类型数据*/
 	int size;               /*有效元素个数*/ 
 };
 
 //动态顺序表
-typedef struct seqList {
+typedef struct SeqList {
 	SQLDataType* data;   //数组指针
 	int size;            //有效元素的个数
 	int capacity;        //数组的容量
-}seqList;
+}SeqList;
+
+//容量检查
+void SeqListcapacity(SeqList* sl);
 
 //初始化顺序表
-void initseqList(seqList* sl);
+void InitseqList(SeqList* sl);
 
 //增删查改
 //尾插
-void pushBack(seqList* sl, SQLDataType val);
+void PushBack(SeqList* sl, SQLDataType val);
+
+//头插
+void PushFront(SeqList* sl, SQLDataType val);
+
+//任意位置插入元素
+void Insert(SeqList* sl, int pos, SQLDataType val);
+
+//尾删
+void PopBack(SeqList* sl);
+
+//头删
+void PopFront(SeqList* sl);
+
+//删除任意位置的数据
+void Erase(SeqList* sl, int pos);
+
+//打印
+void print(SeqList* sl);
 
