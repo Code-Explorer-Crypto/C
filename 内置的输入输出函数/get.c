c989dfc89aaacf5cbe1cb342f83affb233499dd0
hu <hurk7106235414@163.com>
2020-12-12 09:47:49 +0800

    Create get.c

---
+++ 内置的输入输出函数/get.c
@@ -0,0 +1,77 @@
+//#include <stdio.h>
+//int main()
+//{
+//  /*  do {*/
+//        /*c = getchar();*/
+//    int c = getchar();
+//    putchar(c);
+//  
+//        
+//   /* } while (c != '.');*/
+//    return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//
+//    char a[] = { 0 };
+//    do{
+//        scanf("%s", &a);
+//    } while (a != '.');
+//    return 0;
+//}
+
+
+///* getc example: money counter */
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//    FILE* pFile;
+//    pFile = fopen("myfile.txt", "r");
+//    if (pFile == NULL) perror("Error opening file");
+//    else
+//    {
+//        printf("%d\n", getc(pFile));
+//        printf("%d\n", getc(pFile));
+//        fclose(pFile);
+//    }
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//    int c;
+//        getchar();
+//        printf("%d", c); 
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char string[256];
+//	printf("Insert your full address: ");
+//	gets(string);     // warning: unsafe (see fgets instead)
+//	printf("Your address is: %s\n", string);
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//    FILE* pFile;
+//    pFile = fopen("myfile.txt", "r");
+//    if (pFile == NULL) perror("Error opening file");
+//    else
+//    {
+//        printf("%d\n", getc(pFile));
+//        printf("%d\n", getc(pFile));
+//        fclose(pFile);
+//    }
+//    return 0;
+//}
