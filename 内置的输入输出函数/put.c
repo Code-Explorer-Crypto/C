de516bfdd4a79a1a194fc120a2f53846267d6b58
hu <hurk7106235414@163.com>
2020-12-12 09:47:46 +0800

    Create put.c

---
+++ 内置的输入输出函数/put.c
@@ -0,0 +1,42 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//    FILE* pFile;
+//    char c;
+//    pFile = fopen("myfile.txt", "r");
+//   c = 'a';
+//        putc(c, pFile);
+//        printf("%d",getc(pFile));
+//    /*fclose(pFile);*/
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main(){
+//	char c;
+//	for (c = 'A'; c <= 'Z'; c++) /*putchar(c);*/
+//	printf("%d", putchar(c));
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//    FILE* pFile;
+//    char c; 
+//    pFile = fopen("myfile.txt", "wt");
+//    for (c = 'A'; c <= 'Z'; c++) {
+//        printf("%d",puts(c, pFile));
+//    }
+//    fclose(pFile);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char string[] = "Hello world!";
+//	printf("%d",puts(string));
+//}
