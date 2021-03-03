d6d3b0190d6fe133314d7624f7c77ea12ec86936
hu <hurk7106235414@163.com>
2021-03-03 10:56:44 +0800

    Create test1.c

---
+++ 顺序表.test2/test1.c
@@ -0,0 +1,20 @@
+//#include <stdio.h>
+//int main() {
+//	int i = 0, j = 0;
+//	int a[6][7];
+//	int n = 6;
+//	/*a[i][j] = ++n;
+//	printf("%d", a[i][j]);*/
+//	while (--n) {
+//		j=j+1;
+//		printf("%d", n);
+//	}
+//	printf("%d", j);
+////	for (int l = 0;l < 5;++l) {
+////		i = i + 1;
+////		printf("%d", l);
+////	}
+////	/*printf("%d", i);*/
+////	
+//	return 0;
+//}
