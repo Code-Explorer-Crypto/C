d5e1442aa6fafa70895531855a628d2480c1920b
hu <hurk7106235414@163.com>
2021-03-16 09:00:19 +0800

    Create 猜数字.c

---
+++ 猜数字/猜数字.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main() {
+	int a = 0, b = 0;
+	srand((unsigned int)time(NULL));
+	int r = rand() % 100 + 1;
+	printf("请猜数字:");
+	scanf("%d", &b);
+	if (b > r) {
+		printf("猜大了\n");
+	}
+	else if (b < r) {
+		printf("猜小了\n");
+	}
+	else if (b == r) {
+		printf("猜对了");
+	}
+	while (b != r) {
+		printf("是否继续进行游戏(按任意键表示继续游戏/1表示溜了溜了)\n");
+		scanf("%d", &a);
+		if (a == 1) {
+			break;
+		}
+		else {
+			printf("请猜数字:");
+			scanf("%d", &b);
+			if (b > r) {
+				printf("猜大了\n");
+			}
+			else if (b < r) {
+				printf("猜小了\n");
+			}
+		}
+	}
+	if (b == r) {
+		printf("猜对了");
+	}
+	return 0;
+}
