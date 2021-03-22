7a6b43eb04d2e2e42640faf8a5baa584da891ce1
hu <hurk7106235414@163.com>
2021-03-22 23:07:51 +0800

    Update 在一个整形有序数组中查找具体的某个数.c

--- 在一个整形有序数组中查找具体的某个数/在一个整形有序数组中查找具体的某个数.c
+++ 在一个整形有序数组中查找具体的某个数/在一个整形有序数组中查找具体的某个数.c
@@ -1,31 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define n 9
- int main() {
-	 int arr[n] = { 0, 1, 2, 5, 7, 9, 11, 12, 13 };
-	 //printf("%d", sizeof(arr)/4);
-	 //折半查找法
-	 int k = 0;
-	 printf("请输入要查找的值: ");
-	 scanf("%d", &k);
-	 int top = 0;
-	 int bot = n - 1;
-	 int j = (bot - top) / 2;
-	 while (arr[j] != k && top != bot) {
-		 if (arr[j] < k) {
-			 top = j + 1;
-		     j = j + (bot - top) / 2;
-		 }
-		 else if (arr[j] > k) {
-			 bot = j - 1;
-			 j = (bot - top) / 2;
-		 }
-	 }
-	 if (top == bot) {
-		 printf("您要找的数没在里面");
-	 }
-	 if (arr[j] != k) {
-		 printf("您要找的数的是第%d个",j);
-	 }
-	 return 0;
- }
+//#define n 9
+// int main() {
+//	 int arr[n] = { 0, 1, 2, 5, 7, 9, 11, 12, 13 };
+//	 //printf("%d", sizeof(arr)/4);
+//	 //折半查找法
+//	 int k = 0;
+//	 printf("请输入要查找的值: ");
+//	 scanf("%d", &k);
+//	 int top = 0;
+//	 int bot = n - 1;
+//	 int j = (bot - top) / 2;
+//	 while (arr[j] != k && top != bot) {
+//		 if (arr[j] < k) {
+//			 top = j + 1;
+//		     j = j + (bot - top) / 2;
+//		 }
+//		 else if (arr[j] > k) {
+//			 bot = j - 1;
+//			 j = (bot - top) / 2;
+//		 }
+//	 }
+//	 if (top == bot) {
+//		 printf("您要找的数没在里面");
+//	 }
+//	 if (arr[j] != k) {
+//		 printf("您要找的数的是第%d个",j);
+//	 }
+//	 return 0;
+// }
+
+int main() {
+	int  x = y = 1;
+	printf("a=%d%d", x, y);
+}
