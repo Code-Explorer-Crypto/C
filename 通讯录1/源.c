a3a172e652fb9ec43cbb66b0ca8ebea3746411ea
hu <hurk7106235414@163.com>
2021-05-08 16:33:49 +0800

    Create 源.c

---
+++ 通讯录1/源.c
@@ -0,0 +1,28 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main() {
+//	int a = 10, b = 20;
+//	int* p = &a;     // 定义一个整形的指针 p 并且 p 指向了 a 的地址
+//	printf("%d\n", *p); // 一经解引用,即为取p(即为&a)的解引用,即取a地址的内容物
+//	int** pp = &p;   // 定义一个整型的指针 pp 并且 pp 指向了 p 的地址
+//	printf("%d", **pp);// 一经解引用,即为取pp(即为&p)的解引用,即取p地址的内容物
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//struct Point {
+//	int x;
+//	int y;
+//};
+//
+//int main() {
+//	struct Point s = { 3, 5 };
+//	printf("%d %d", s.x, s.y);
+//	return 0;
+//}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(){
+
+}
