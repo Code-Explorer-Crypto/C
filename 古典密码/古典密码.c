7ba44f44e4183b41e154605feb261a9d2124bb89
hu <hurk7106235414@163.com>
2021-07-07 14:28:17 +0800

    Create 古典密码.c

---
+++ 古典密码/古典密码.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define MAX 100
+int main() {
+	int a = 0;
+	int arr[MAX] = { 0 };
+	//ด๓ะด
+	if (arr[0] >= 65 &&arr[0] <= 90) {
+		for (int i = 0;i < sizeof(arr);i++) {
+
+		}
+	}
+	//ะกะด
+	else if (arr[0] >= 97 && arr[0] <= 122) {
+
+	}
+  }	
