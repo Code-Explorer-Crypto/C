6d00c694eab7dc8e9029bbf2f2d860b3eeb13fec
hu <hurk7106235414@163.com>
2021-07-07 14:25:53 +0800

    Create i++和++i.c

---
+++ i++和++i/i++和++i.c
@@ -0,0 +1,18 @@
+//#include <stdio.h>
+// int main(){
+//	 int k = 0;
+//	 int j = 0;
+//	 for (int i = 0;i < 5;i++) {
+//		 printf("%d", i);
+//		/* k++;
+//		 printf("%d", k);*/
+//		 printf("%d", k++);
+//   }
+//	 for (int i = 0;i < 5;++i) {
+//		 printf("\n%d", i);
+//		 /*++j;
+//		 printf("%d", j);*/
+//		 printf("%d", ++j);
+//	 }
+// }
+
