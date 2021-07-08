424e676db761e493a41ba13c7863b30d6495f855
hu <hurk7106235414@163.com>
2021-07-08 09:22:15 +0800

    Create str.c

---
+++ Str,文件操作,动态内存分配/str.c
@@ -0,0 +1,17 @@
+//getchar
+//#include<stdio.h>
+//int main() {
+//	printf("%d\n", getchar());
+//	printf("%d\n", getchar());
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main() {
+//	int a = getchar();
+//	printf("%d", a);
+//	printf("%d", getchar());
+//}
+
+
+
